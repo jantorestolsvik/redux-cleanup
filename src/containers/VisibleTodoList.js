17e10709982d9e96454b95b32e92cd9052cb6a6d
Jan Tore <jtstolsvik@gmail.com>
2017-03-02 23:26:06 +0100

    enkel versjon av mapDispatchToProps

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -19,11 +19,9 @@ const mapStateToProps = (state) => ({
     todos: getVisibleTodos(state.todos, state.visibilityFilter)
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    onTodoClick(id) {
-        dispatch(toggleTodo(id));
-    }
-});
+const mapDispatchToProps = {
+    onTodoClick: toggleTodo
+};
 
 const VisibleTodoList = connect(
     mapStateToProps,
