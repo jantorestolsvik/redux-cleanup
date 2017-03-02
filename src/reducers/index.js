02a0c9e02c1f2bd10ce0d3de1670af13c7becbd8
Jan Tore <jtstolsvik@gmail.com>
2017-03-02 23:45:37 +0100

    la selectors på samme sted som reducere

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
+import todos, * as fromTodos from './todos';
 import visibilityFilter from './visibilityFilter';
 
 const todoApp = combineReducers({
     todos,
     visibilityFilter
 });
+
+export const getVisibleTodos = (state) => fromTodos.getVisibleTodos(state.todos, state.visibilityFilter);
 
 export default todoApp;
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -19,6 +19,19 @@ const todo = (state, action) => {
     }
 };
 
+export const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_COMPLETED':
+            return todos.filter(t => t.completed);
+        case 'SHOW_ACTIVE':
+            return todos.filter(t => !t.completed);
+        default:
+            throw new Error(`Unknown filter: ${filter}.`);
+    }
+};
+
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
